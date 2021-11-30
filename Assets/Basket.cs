9e753dea55dba42a02032eec5cf12817b7d28405
Henrik Himmelreich <hepahi@web.de>
2021-11-30 19:07:48 +0100

    Price and moneySpent counter
    Added price to cucumbers and counter for money spent that increases when goods are put into basket.

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -6,6 +6,8 @@ using Valve.VR.InteractionSystem;
 
 public class Basket : MonoBehaviour
 {
+    public float totalAmountSpent = 0f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,24 +22,32 @@ public class Basket : MonoBehaviour
 
     IEnumerator ExecuteAfterTime(float time, GameObject good)
     {
+        // short delay to let item settle
         yield return new WaitForSeconds(time);
         
+        // no more interaction possible
         Destroy(good.GetComponent<Throwable>());
         Destroy(good.GetComponent<Interactable>());
         Destroy(good.GetComponent<Rigidbody>());
 
+        // make child of basket
         good.transform.parent = transform;
     }
     
     private void OnCollisionEnter(Collision other)
     {
+        // reference to gameobject of collision
         GameObject good = other.gameObject;
         
+        // if item is a good
         if (good.tag == "Goods")
         {
+            // put item in basket
             StartCoroutine(ExecuteAfterTime(0.5f, good));
-            
-            
+
+            totalAmountSpent += good.GetComponent<Goods>().price;
+            good.GetComponent<Goods>().price = 0f;
+
         }
     }
 }
